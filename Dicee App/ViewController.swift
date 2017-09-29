5b68ef57c61f395376cc81c47661f067a9fddc91
kryptoslex <kryptoslex@gmail.com>
2017-09-29 11:06:44 +0800

    updates

--- Dicee App/ViewController.swift
+++ Dicee App/ViewController.swift
@@ -13,10 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var dice1: UIImageView!
     @IBOutlet weak var dice2: UIImageView!
     
+    var diceValueIndex1: Int = 0
+    var diceValueIndex2: Int = 0
     
+    var diceImages: [String] = ["dice1","dice2","dice3","dice4","dice5","dice6"]
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        randomize()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,6 +32,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButton(_ sender: Any) {
+
+        randomize()
+        print("dice 1 = \(diceValueIndex1) - dice 2 = \(diceValueIndex2)")
+        
+//        diceValueIndex1 = Int(arc4random_uniform(5)) + 1
+//        diceValueIndex2 = Int(arc4random_uniform(5)) + 1
+//
+//
+//        dice1.image = UIImage(named: "dice\(diceValueIndex1)")
+//        dice2.image = UIImage(named: "dice\(diceValueIndex2)")
+        
+    }
+    
+    func randomize()  {
+        
+        diceValueIndex1 = Int(arc4random_uniform(6))
+        diceValueIndex2 = Int(arc4random_uniform(6))
+        
+        dice1.image = UIImage(named: diceImages[diceValueIndex1])
+        dice2.image = UIImage(named: diceImages[diceValueIndex2])
     }
     
 }
