89d3ddf83a764915a8089977a0d45140683aca2c
kryptoslex <kryptoslex@gmail.com>
2017-09-29 10:22:06 +0800

    updates

--- Dicee App/ViewController.swift
+++ Dicee App/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButton(_ sender: Any) {
+    }
+    
 }
 
